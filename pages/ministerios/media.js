12b9fb2c69ba78ef89c27f90b69910e384734dba
walterrcruz1981 <walterrcruz@yahoo.com>
2022-11-16 11:53:45 -0800

    added media info

--- pages/ministerios/media.js
+++ pages/ministerios/media.js
@@ -6,7 +6,8 @@ const domain = 'https://dih6tqxrn8ffv.cloudfront.net/'
 const video = 'mediaVideo.mp4'
 const heroImage = 'fotos/media/mediaTeam.jpg'
 const heroVideo = 'heroVideo.mp4'
-
+const misionImage = 'https://images.unsplash.com/photo-1502101872923-d48509bff386?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1932&q=80'
+const visionImage = 'https://images.unsplash.com/photo-1622202210941-5fc06f1d581a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80'
 function media() {
     return (
         <MediaContainer>
@@ -18,10 +19,27 @@ function media() {
                 </div>
                 <video objectFit='cover' autoPlay muted src={domain + heroVideo}></video>
             </div>
+            <div className="card-container">
+                <div className="card flex-center-column text-shadow">
+                    <h1>Visión</h1>
+                    <p>{Media.vision}</p>
+                    <Image layout="fill" objectFit="cover" src={visionImage} alt='transmission en vivo'></Image>
+                </div>
+                <div className="card flex-center-column">
+                    <h1>Misión</h1>
+                    <p>{Media.mision}</p>
+                    <Image layout="fill" objectFit="cover" src={misionImage} alt='transmission en vivo'></Image>
+                </div>
+            </div>
             <div className="video-container">
                 <h1>¿Que Hacemos?</h1>
                 <hr />
                 <video autoPlay muted src={domain + video}></video>
+                <div className="duties">
+                    {Media.duties?.map((duty, index) => (
+                        <h1 className="box-shadow" key={index}>{duty}</h1>
+                    ))}
+                </div>
             </div>
 
         </MediaContainer>
@@ -36,6 +54,8 @@ margin: 0 auto;
     padding: 38% 0 0;
     width: 100%;
     height: 60em;
+    border-radius: .6em;
+    overflow: hidden;
     video{
         position: absolute;
         z-index: -1;
@@ -46,13 +66,41 @@ margin: 0 auto;
     }
     .text-content{
         width: 100%;
-        margin-top: -6em;
+        margin-top: -3.5em;
         margin-left: 25%;
         color: white;
         font-size: 2rem;
         span{
-            color: #20aaa3;
-            font-size: 6rem;
+            color: #57b3ff;
+            font-size: 3rem;
+        }
+    }
+}
+.card-container{
+        display: grid; 
+        margin: 1.8em 0;
+        gap: 1em;
+        grid-template-columns: repeat( auto-fit, minmax(360px, 1fr) );
+        
+    .card{
+        position: relative;
+        height: 30em;
+        border-radius: .6em;
+        overflow: hidden;
+        p, h1{
+            margin: 1em;
+            color: white;
+            z-index: 1;
+            background-color: #000000ae;
+        }
+        img{
+            top: 0;
+            filter: contrast(1.8);
+            :hover{
+                transform: scale3d(1.2,1.1,1.8) skewY(2deg);
+            
+            }
+            transition: all 800ms ease-in-out;
         }
     }
 }
@@ -61,6 +109,18 @@ margin: 0 auto;
         width: 100%;
         height: 100%;
         object-fit: cover;
+    }
+    .duties{
+        display: grid; 
+        margin: 1.8em 0;
+        gap: 1em;
+        grid-template-columns: repeat( auto-fit, minmax(360px, 1fr) );
+        h1{
+            background-color: #66adff;
+            padding: .5em;
+            text-align: center;
+            color: white;
+        }
     }
 
 }
